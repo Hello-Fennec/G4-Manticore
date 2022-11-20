1e1584ee59ed5bf74b1416f1a1aaef3e432b706f
P <dpheeraphat2547@gmail.com>
2022-11-20 12:57:43 +0700

    edit navbar
    finish but not responsive

--- manticore_mantijai/src/App.js
+++ manticore_mantijai/src/App.js
@@ -5,8 +5,10 @@ import Nav from './Navbar';
 function App() {
   return (
     <div className="App">
-    <div className='bg-gradient-to-r from-yellow-600 to-lime-700 ... w-full h-screen'></div>
-         
+      <div className='bg-gradient-to-r from-yellow-600 to-lime-700 ... w-full h-screen'>
+          <Nav />
+      </div>
+
     </div>
   );
 }
--- manticore_mantijai/src/Navbar.jsx
+++ manticore_mantijai/src/Navbar.jsx
@@ -1,7 +1,23 @@
-const Nav = () => {
+import React from "react";
+
+const Nav = () => {   
     return (
-        <div>
-            Manticore
+        <div className="shadow-md w-full fixed top-0 left-0">
+            <div className="md:flex items-center justify-between py-4 md:px-10 md:px-7">
+                <div className=" ml-8 font-bold text-2xl cursor-pointer flex items-center text-white">
+                    MANTICORE <br/> Mantijai                   
+                </div>
+              <ul className="md:flex md:items-center md:max-xl mr-20">
+                <li className="md:max-xl space-x-20  text-xl text-white ">
+                <a href="#">HOME</a>
+                <a>HISTORY</a>
+                <a>DRAWING</a>
+                <a>GALLERY ART</a>
+                <a>QUIZ</a>
+                <a>ABOUT</a>
+                </li>                
+              </ul>
+            </div>
         </div>
 
 
