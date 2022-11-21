fd4347724bdcac71f797443418895dde72ff7f61
P <dpheeraphat2547@gmail.com>
2022-11-21 20:04:46 +0700

    start home component

--- manticore_mantijai/src/App.js
+++ manticore_mantijai/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './component/Navbar';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">      
       <div className='bg-orange-100 w-full h-screen'>
           <Nav />
+          <Home />
       </div>
 
     </div>
---
+++ manticore_mantijai/src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home () {
+    return (
+
+        <div>
+            <h3>GET TO KNOW MANTICORE</h3>
+            <h2>LEGENDARY CREATURE</h2>
+            <p>A magical</p>
+        </div>
+
+    );
+}
+
+export default Home;
