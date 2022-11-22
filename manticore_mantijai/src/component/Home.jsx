b31dc8af63e1f23a1921bfbc47049188c0de9757
P <dpheeraphat2547@gmail.com>
2022-11-22 09:45:07 +0700

    add picture

--- manticore_mantijai/src/component/Home.jsx
+++ manticore_mantijai/src/component/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-function Home () {
+function Home() {
     return (
-
         <div>
-            <h3>GET TO KNOW MANTICORE</h3>
-            <h2>LEGENDARY CREATURE</h2>
-            <p>A magical</p>
+            <div  className="mt-96 ml-24 float-left bg-gradient-to-r from-amber-200 ... py-12 px-12 pr-12 ">
+                <h3 className="text-4xl text-transparent font-poppins font-bold bg-clip-text bg-gradient-to-r from-emerald-900 to-green-600 ">GET TO KNOW MANTICORE</h3>
+                <br />
+                <h2 className="text-5xl text-transparent font-poppins font-bold bg-clip-text bg-gradient-to-r from-emerald-900 to-green-600 ">LEGENDARY CREATURE</h2>
+                <br />
+                <p className="font-poppins">A magical beast and one of the wizarding world's most dangerous creatures.</p>
+            </div>
+
+            <img className = "w-96  pt-72 float-right mr-60" src="../img/home.png" alt="homepicture" />
         </div>
+
+
 
     );
 }
