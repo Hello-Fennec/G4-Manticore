1b584e3e11c0b8cad6cac2ecf65c324975ded641
Phee 47 <dpheeraphat2547@gmail.com>
2022-11-24 22:06:40 +0700

    set before do quiz

--- manticore_mantijai/src/App.js
+++ manticore_mantijai/src/App.js
@@ -4,20 +4,13 @@ import Nav from './component/Navbar';
 import Home from './component/Home';
 import Appear from './component/Appearance';
 import History from './component/History';
-import { createContext, useState } from "react";
-import Menu from "./component/Menu";
-import Quiz from "./component/Quiz";
-import Score from "./component/Score";
 
-{/* Context API (ให้บริการเกี่ยวกับข้อมูลสถานะของตัว Application )*/ }
-export const DataContext = createContext()
+
+
 
 
 function App() {
-  {/* ส่วนของ State สำหรับจัดเก็บสถานะภายในแอพ */ }
-  const [appState, setAppState] = useState("menu");
-  {/* ส่วนของ state สำหรับจัดเก็บคะแนน */ }
-  const [score, setScore] = useState(0);
+
 
   return (
     <div className="App">
