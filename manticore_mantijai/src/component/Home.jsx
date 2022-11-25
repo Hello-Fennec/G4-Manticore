2b740ff514035379a42ad34f42a89353332bb25d
Phee 47 <dpheeraphat2547@gmail.com>
2022-11-25 16:30:26 +0700

    add file

--- manticore_mantijai/src/component/Home.jsx
+++ manticore_mantijai/src/component/Home.jsx
@@ -5,16 +5,9 @@ import "./Home.css";
 function Home() {
     return (
         <div>
-            <div className="mt-64 ml-24 float-left bg-gradient-to-r from-amber-200 ... py-12 px-12 pr-12 ">
-                <h3 className="text-4xl text-transparent font-poppins font-bold bg-clip-text bg-gradient-to-r from-emerald-900 to-green-600 ">GET TO KNOW MANTICORE</h3>
-                <br />
-                <h2 className="text-5xl text-transparent font-poppins font-bold bg-clip-text bg-gradient-to-r from-emerald-900 to-green-600 ">LEGENDARY CREATURE</h2>
-                <br />
-                <p className="font-poppins">A magical beast and one of the wizarding world's most dangerous creatures.</p>
-            </div>
-
-            <img className="w-1/4  mt-56 float-right mr-16" src="../img/home.png" alt="homepicture" />
-            
+            <div>
+                <img id="homeimage"src="../img/Home_Manticore.png"/>
+            </div>            
             <div className="clouds">
                 <img src="../img/cloud1.png" className="cloud1" />
             </div>
