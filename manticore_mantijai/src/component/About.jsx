6bac0ff72ca7802f1bba94ab1466ae7888d64e2d
Phee 47 <dpheeraphat2547@gmail.com>
2022-11-25 14:37:13 +0700

    Finish AboutUs

--- manticore_mantijai/src/component/About.jsx
+++ manticore_mantijai/src/component/About.jsx
@@ -3,8 +3,24 @@ import './About.css';
 
 function About() {
     return(
-        
-        <div>
+
+        <div id="AboutUs">
+            <div id="AboutTopic">
+                <img src="../img/AboutUs.png" id="AbouUsTopic"/>                
+            </div>
+            
+
+            <div id="FrontEnd">
+                <img src="../img/FronEnd.png" />
+            </div>
+
+            <div id="DesignInfra">
+                 <img src="../img/DesignInfra.png"  />
+            </div>
+
+            <div id="Footer">
+                <img src="../img/Footer.png" />
+            </div>
 
         </div>
 
